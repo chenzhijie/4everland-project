b1bba669b48a7f4c2b658c9ee6b8a3ab86c3a33a
chenzhijie <chenzhijie@onchain.com>
2021-09-18 11:33:20 +0800

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,11 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+      Fist Project On 4everland
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
 
 export default {
   name: 'app',
